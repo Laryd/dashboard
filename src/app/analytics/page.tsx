cd9b71f2171fb9d0c4586042754d6d8326cb3f64
Laryd <hillarymuboka@gmail.com>
2024-02-18 16:48:11 +0300

    tremor dashboard

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -1,16 +1,36 @@
-import AnalyticsDashboard from '@/components/AnalyticsDashboard'
-import { analytics } from '@/utils/analytics'
-import React from 'react'
+import AnalyticsDashboard from "@/components/AnalyticsDashboard";
+import { getDate } from "@/utils";
+import { analytics } from "@/utils/analytics";
 
 const Page = async () => {
-    const pageview = await analytics.retrieveDays('pageview', 2)
-  return ( 
-    <div className="min-h-screen w-full py-12 flex justify-center">
-        <div className="relative w-full max-w-6xl mx-auto text-white">
-            <AnalyticsDashboard/>
-        </div>
-    </div>
-  )
-}
+  const TRACKING_DAYS = 7;
+  const pageviews = await analytics.retrieveDays("pageview", TRACKING_DAYS);
 
-export default Page
+  const totalPageviews = pageviews.reduce((acc, curr) => {
+    return (
+      acc +
+      curr.events.reduce((acc, curr) => {
+        return acc + Object.values(curr)[0]!;
+      }, 0)
+    );
+  }, 0);
+
+  const avgVisitorsPerDay = (totalPageviews / TRACKING_DAYS).toFixed(1);
+  const amtVisitorsToday = pageviews
+    .filter((ev) => ev.date === getDate())
+    .reduce((acc, curr) => {
+      return (
+        acc +
+        curr.events.reduce((acc, curr) => acc + Object.values(curr)[0]!, 0)
+      );
+    }, 0);
+  return (
+    <div className="min-h-screen w-full py-12 flex justify-center">
+      <div className="relative w-full max-w-6xl mx-auto text-white">
+        <AnalyticsDashboard avgVisitorsPerDay={avgVisitorsPerDay} amtVisitorsToday={amtVisitorsToday}/>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
--- src/components/AnalyticsDashboard.tsx
+++ src/components/AnalyticsDashboard.tsx
@@ -1,9 +1,34 @@
+"use client"
+import { Card } from "@tremor/react";
 
-
-const AnalyticsDashboard = () => {
-  return (
-    <div>AnalyticsDashboard</div>
-  )
+interface AnalyticsDashboardProps {
+    avgVisitorsPerDay: string,
+    amtVisitorsToday: number
 }
 
-export default AnalyticsDashboard
+const AnalyticsDashboard = ({avgVisitorsPerDay, amtVisitorsToday}: AnalyticsDashboardProps) => {
+  return (
+    <div className="flex flex-col gap-6">
+      <div className="grid w-full mx-auto grid-cols-1 sm:grid-cols-2 gap-6">
+        <Card className="w-full mx-auto max-w-xs">
+          <p className="text-tremor-default text-dark-tremor-content">
+            Avg. visitors/day
+          </p>
+          <p className="text-3xl text-dark-tremor-content-strong font-semibold">
+            {avgVisitorsPerDay}
+          </p>
+        </Card>
+        <Card className="w-full mx-auto max-w-xs">
+          <p className="text-tremor-default text-dark-tremor-content">
+            Visitors Today
+          </p>
+          <p className="text-3xl text-dark-tremor-content-strong font-semibold">
+            {amtVisitorsToday}
+          </p>
+        </Card>
+      </div>
+    </div>
+  );
+};
+
+export default AnalyticsDashboard;
